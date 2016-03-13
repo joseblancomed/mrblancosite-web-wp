d422caf820a24d21bbb59eecf41f210c365c7088
Jose Miguel Blanco Medina <jomiblamed@gmail.com>
2016-03-13 19:06:15 +0000

    Code update in a database
    Updating register

--- db.php
+++ db.php
@@ -16,7 +16,7 @@ class dbConnection
 		if($cDb)
 		{
 			$this->connection = mysqli_connect("localhost","root","","contactNote");
-			$tbl = mysqli_query($this->connection,"create table if not exists contactPeople(id int auto_increment, name varchar(70), address varchar(70), telephone varchar(70), primary key(id));");
+			$tbl = mysqli_query($this->connection,"create table if not exists contactPeople(id int auto_increment, name varchar(70), address varchar(70), telephone varchar(15),other_number varchar(15), primary key(id));");
 			if(!($tbl))
 				echo "<script language='JavaScript'>alert('Table no created');</script>";
 		}
@@ -24,15 +24,13 @@ class dbConnection
 			echo "<script language='JavaScript'>alert('Database no created');</script>";
 		
 	}
-
-	// When you have the class created, you should start to program the functions
-
-	function addRegister($fullName,$fullAddress,$numTele)
+	
+	function addRegister($fullName,$fullAddress,$numTele,$otherNum=0)
 	{
 		$this->connection = mysqli_connect("localhost","root","","contactNote");
 		//echo $this->connection;
 		if (($fullName=="") && ($numTele=="" || $fullAddress==""))
-			echo "<script language='JavaScript'>alert('The field telephone or address are mandatory');</script>";
+			echo "<script language='JavaScript'>alert('The field telephone or address are mandatory, almost one of them');</script>";
 		else
 		{
 			$regExists = mysqli_query($this->connection, "select id from contactPeople where name='".$fullName."' and telephone='".$numTele."' or address= '".$fullAddress."';");
@@ -45,46 +43,118 @@ class dbConnection
 			}
 			else
 				echo "<script language='JavaScript'>alert('The contact already exists in the database');</script>";
+				
 		}
 		
 	}
-	
-	function readRegisters()
+
+	function deleteRegister()
 	{
-		echo "<form action='#' method='POST'><select name='chooseId'>";
+		echo "<fieldset id='viewTable'>";
+		
+		$this->connection = mysqli_connect("localhost","root","","contactNote");
+		$eachreg = mysqli_query($this->connection, "select id,name,address,telephone,other_number from contactpeople;");
+		
+		echo "<form action='#' method='post' id='viewform'>";
+
+		echo "<br/><input type='hidden' name='chCont' id='chCont' readonly />";
+		echo "<br/><br/>Choose the contact:<br/><select id='chooseid' onchange=\"document.getElementById('chCont').value = document.getElementById('chooseid').value\">";
+		
+		if (mysqli_num_rows($eachreg)>0)
+		{
+			echo "<option value='null'>-- choose one --</option>";
+			while ($pos = mysqli_fetch_array($eachreg,MYSQLI_BOTH))
+			{
+				echo "<option value=".$pos['id'].">".$pos['name']."</option>";
+			}
+			
+			echo "</select><input type='submit' value='Delete' name='deleteOne'/>";
+			//alert(document.getElementById('chooseid').value)\" />";
+		}
+		else
+			echo "<option>The contactnote is empty</option></select></div>";
+		echo "</form></fieldset>";
+	
+	}
+	
+	function printTable()
+	{
 		$this->connection = mysqli_connect("localhost","root","","contactNote");
 		//echo $this->connection;
-		$eachReg = mysqli_query($this->connection, "select id,name,address,telephone from contactPeople;");
-		$eachRegTab = mysqli_query($this->connection, "select id,name,address,telephone from contactPeople;");
+		$eachRegTab = mysqli_query($this->connection, "select id,name,address,telephone,other_number from contactPeople;");
 		
-		if (mysqli_num_rows($eachReg)>0)
+		if (mysqli_num_rows($eachRegTab)>0)
 		{
-			while ($item = mysqli_fetch_array($eachReg,MYSQL_BOTH))
-				echo "<option value=".$item['id'].">".$item['name']." > ".$item['telephone']." > ".$item['address']."</option>";
-			echo "<input type='submit' value='Delete' name='deleteIt' /></select></form>";
-			
-			echo "<table border=2><tr><td>Name</td><td>Address</td><td>Telephone</td></tr>";
-			while ($regSaved = mysqli_fetch_array($eachRegTab,MYSQL_BOTH))
-				echo "<tr><td>".$regSaved['name']."</td><td>".$regSaved['address']."</td><td>".$regSaved['telephone']."</td></tr>";
+			echo "<table border=0 width='100%'><tr><th>Name</th><th>Address</th><th>Telephone</th><th>Other number</th></tr>";
+			while ($regSaved = mysqli_fetch_array($eachRegTab,MYSQLI_BOTH))
+				echo "<tr><td>".$regSaved['name']."</td><td>".$regSaved['address']."</td><td>".$regSaved['telephone']."</td><td>".$regSaved['other_number']."</td></tr>";
 			echo "</table>";
 		}
 		else
-			echo "<option>The contactBook is empty</option></select>";
+			echo "<option>The contactBook is empty, PLEASE MAKE FRIENDS</option></select>";
 		
-		
+		//echo "<br/><br/><input type='button' value='Close table' onClick='closeView();' />";
 	}
-	
-	function deleteRegister($id)
+
+	function deleteContact($id)
 	{
+		//echo "<script language='JavaScript'>alert('".$id."');</script>";
 		$this->connection = mysqli_connect("localhost","root","","contactNote");
-		/*echo $this->connection;
-		echo "<script language='JavaScript'>alert('".$id."');</script>";*/
+		//echo $this->connection;
+		//echo "<script language='JavaScript'>alert('".$id."');</script>";
 		$delReg = mysqli_query($this->connection, "delete from contactPeople where id=".$id.";");
 		if (!($delReg))
 			echo "<script language='JavaScript'>alert('Had an issue deleting the contact');</script>";
-		readRegisters();
 	}
 	
+	function updateContact($id, $nName, $nAddress, $nPhone, $nExtraPhone)
+	{	
+		
+		//echo "<script language='JavaScript'>alert('".$id." ' +  'Name: ".$nName." ' + 'Address: ".$nAddress." ' + 'New Phone: ".$nPhone." ' + ' - ".$nExtraPhone."');</script>";
+		$this->connection = mysqli_connect("localhost","root","","contactNote");
+		//echo $this->connection;
+		//echo "<script language='JavaScript'>alert('".$id."');</script>";
+		$updReg = mysqli_query($this->connection, "update contactPeople set name='".$nName."', address='".$nAddress."', telephone='".$nPhone."', other_number='".$nExtraPhone."' where id=".$id.";");
+		if (!($updReg))
+			echo "<script language='JavaScript'>alert('Had an issue updating the contact');</script>";
+	}
+	
+	function updateRegister()
+	{
+		echo "<fieldset id='viewTable'>";
+		
+		$this->connection = mysqli_connect("localhost","root","","contactNote");
+		$eachreg = mysqli_query($this->connection, "select id,name,address,telephone,other_number from contactpeople;");
+		
+		echo "<form action='#' method='post' id='viewform'>";
+
+		echo "<br/><input type='hidden' name='chOne' id='chOne' readonly />";
+		echo "<br/><br/>Choose the contact:<br/><select id='chooseCt' onchange=\"document.getElementById('chOne').value = document.getElementById('chooseCt').value\">";
+		
+		if (mysqli_num_rows($eachreg)>0)
+		{
+			echo "<option value='null'>-- choose one --</option>";
+			while ($pos = mysqli_fetch_array($eachreg,MYSQLI_BOTH))
+			{
+				echo "<option value=".$pos['id'].">".$pos['name']."</option>";
+			}
+			
+			echo "</select>
+			Name:&nbsp; <input type='text' name='nName' maxlength='70' /><br/><br/>
+
+			Address:&nbsp;<input type='text' name='nAddress' maxlength='70' /><br/><br/>
+
+			Telephone:&nbsp;<input type='text' name='nNumber' maxlength='15' /><br/><br/>
+
+			Add mobile:&nbsp;<input type='checkbox' id='pressIt' checked onClick='change()'/><br/><br/>
+
+			<span id='eNum'>Other number:&nbsp;<input type='text' name='nExtraNum' maxlength='15' /></span>";
+			//alert(document.getElementById('chooseid').value)\" />";
+		}
+		else
+			echo "<option>The notebook is empty</option></select></fieldset>";
+		echo "<input type='submit' value='Update' name='updateOne'/></form></fieldset>";
+	}
 		
 	function __destruct()
 	{
--- index.php
+++ index.php
@@ -1,19 +1,24 @@
 <?php
 	$cFile = include("db.php");
 	if ($cFile)
+	{
 		$person = new dbConnection();
+	}
 	else
 		echo "<script language='JavaScript'>alert('Fail to choose the file PHP');</script>";
+	
 ?>
-
 <html>
 
 <head>
 
 <title>Example databases with Classes</title>
 
-</head>
+<link rel="stylesheet" type="text/css" href="css.css" />
+<script type="text/javascript" src="js/jquery-1.5.1.min.js"></script>
+<script type="text/javascript" src="js.js"></script>
 
+</head>
 <body>
 
 <form action="#" method="POST">
@@ -26,29 +31,47 @@ Address:&nbsp;<input type="text" name="iAddress" maxlength="70" /><br/><br/>
 
 Telephone:&nbsp;<input type="text" name="iNumber" maxlength="15" /><br/><br/>
 
-<input type="submit" value="Add" name="saveIt" /><input type="submit" value="Delete contactBook" name="cBook" /><input type="reset" value="Delete form" />
+Add mobile:&nbsp;<input type="checkbox" id="pressIt" checked onClick='change()'/><br/><br/>
+
+<span id="eNum">Other number:&nbsp;<input type="text" name="iExtraNum" maxlength="15" /></span><br/><br/>
+
+<input type="submit" value="Add" name="saveIt" id="saveIt" onclick='reloadTable();' /><input type="reset" value="Delete form" /><br/><br/>
+<input type="submit" value="Delete contact" name="dContact" /><input type="submit" value="Update contact" name="uContact" />
 
 
 </fieldset>
 
+<fieldset id="tableIC"><legend>Table details</legend>
+<?php $person->printTable(); ?>
+</fieldset>
+
 </form>
+
+<?php
+	
+if (isset($_POST['saveIt']))
+{
+	$person->addRegister($_POST['iName'], $_POST['iAddress'],$_POST['iNumber'],$_POST['iExtraNum']);	
+}
+else if (isset($_POST['dContact']))
+{
+	$person->deleteRegister();
+}
+else if (isset($_POST['uContact']))
+{
+	$person->updateRegister();
+}
+else if (isset($_POST['deleteOne']))
+{
+	$person->deleteContact($_POST['chCont']);
+}
+else if (isset($_POST['updateOne']))
+{
+	$person->updateContact($_POST['chOne'],$_POST['nName'], $_POST['nAddress'],$_POST['nNumber'],$_POST['nExtraNum']);
+}
+
+?>
 
 </body>
 
 </html>
-
-<?php
-if (isset($_POST['saveIt']))
-{
-	$contact = new dbConnection();
-	$contact->addRegister($_POST['iName'], $_POST['iAddress'],$_POST['iNumber']);	
-}
-else if (isset($_POST['cBook']))
-{
-	$person->readRegisters();
-}
-else if (isset($_POST['deleteIt']))
-{
-	$person->deleteRegister($_POST['chooseId']);
-}
-?>
